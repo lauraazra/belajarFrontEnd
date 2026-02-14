edcac3d9c2cddc27ad8dab379aa5c37f2998e599
lauraazra <lazraaprilyanti@gmail.com>
2026-02-14 21:30:16 +0700

    Membuat file untuk DOM Manipulation

---
+++ DOM/DOMManipulation.js
@@ -0,0 +1,19 @@
+/* 
+1. Element Manipulation (Mengubah Isi & Atribut)
+    - innerText vs innerHTML: Cara ganti tulisan atau bahkan nambahin tag HTML baru ke dalam sebuah elemen.
+    - Atribut: Cara ngerubah src gambar, ganti href link, atau nambahin id dan class lewat JS.
+    - Style (CSS): Cara ngerubah tampilan (warna, ukuran, display) langsung via property .style.
+2. Node Manipulation (Membuat & Menghapus)
+    -document.createElement(): Cara bikin elemen baru (misal bikin <li> baru pas user klik tombol).
+    - appendChild() / append(): Cara "nempelin" elemen yang baru dibuat ke dalam model/tree.
+    - removeChild() / remove(): Cara menghapus elemen dari halaman.
+3. Class List (Mainan Class CSS)
+    - classList.add(): Nambahin class CSS.
+    - classList.remove(): Ngilangin class.
+    - classList.toggle(): Ini yang paling keren! Kalau class-nya belum ada, dia tambahin. Kalau udah ada, dia hapus. (Sering dipake buat tombol dark mode atau menu hamburger).
+4. DOM Events (Interaksi User)
+    - click: Apa yang terjadi pas user ngeklik sesuatu?
+    - input / change: Apa yang terjadi pas user ngetik di form?
+    - mouseOver / mouseOut: Efek pas kursor lewat di atas elemen.
+*/
+
