a14494edc11f7a3fedb3849dc64ce660401c7063
lauraazra <lazraaprilyanti@gmail.com>
2026-02-23 00:56:21 +0700

    Belajar Traversal

---
+++ DOM/DOMTraversal.js
@@ -0,0 +1,30 @@
+// const card = document.querySelector('.card');
+// const closeCard = document.querySelector('.close')
+
+// closeCard.addEventListener('click', function () {
+//     card.style.display = 'none'
+// })
+
+// DOM Traversal
+const closeCard = document.querySelectorAll('.close')
+
+// for (let i = 0; i < closeCard.length; i++) {
+//     closeCard[i].addEventListener('click', function (e) {
+//         // closeCard[i].parentElement.style.display = 'none'
+//         e.target.parentElement.style.display = 'none'
+//     })
+// }
+
+closeCard.forEach(function (el) {
+    el.addEventListener('click', function (e) {
+        e.target.parentElement.style.display = 'none'
+    })
+})
+
+/* 
+parentNode, orang tua tapi nodenya keambil (enter)
+parentElement, orang tua
+nextSibling, previousSibling, sodara node
+nextElementSibling, previeousElementSibling, orang tua element
+*/
+
