18f0ac857c725ce123e20347c620fab53833d9f1
lauraazra <lazraaprilyanti@gmail.com>
2026-02-23 01:25:46 +0700

    Event Bubbling

--- DOM/DOMTraversal.js
+++ DOM/DOMTraversal.js
@@ -6,7 +6,7 @@
 // })
 
 // DOM Traversal
-const closeCard = document.querySelectorAll('.close')
+// const closeCard = document.querySelectorAll('.close')
 
 // for (let i = 0; i < closeCard.length; i++) {
 //     closeCard[i].addEventListener('click', function (e) {
@@ -32,9 +32,28 @@ nextElementSibling, previeousElementSibling, orang tua element
 // a itu punya aksi default mengembalikan ke suatu halaman jadi harus di kasi pencegahan aksi default
 
 
-closeCard.forEach(function (el) {
-    el.addEventListener('click', function (e) {
+// closeCard.forEach(function (el) {
+//     el.addEventListener('click', function (e) {
+//         e.target.parentElement.style.display = 'none'
+//         e.preventDefault()
+//         e.stopPropagation()
+//     })
+// })
+
+// // jadi bisa ada 2 eventlistener, stopPropagation itu buat menghalau yang cardsnya
+// const cards = document.querySelectorAll('.card');
+// cards.forEach(function (card) {
+//     card.addEventListener('click', function (e) {
+//         alert('hai');
+//     })
+// })
+
+// tapi cara-cara di atas bakal conflict kalo nanti nambah card baru secara real time, nanti dia ga bisa di close karena eventnya cuma ketulis di yang udah ada duluan aja, makannya pakai orang tua yang bungkus semuanya
+
+const container = document.querySelector('.container');
+container.addEventListener('click', function (e) {
+    if (e.target.className == 'close') {
         e.target.parentElement.style.display = 'none'
         e.preventDefault()
-    })
+    }
 })
