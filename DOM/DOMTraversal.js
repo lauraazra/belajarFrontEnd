74055b889bf5cb28d89322e93023fb59923815b7
lauraazra <lazraaprilyanti@gmail.com>
2026-02-23 01:04:16 +0700

    preventDefault

--- DOM/DOMTraversal.js
+++ DOM/DOMTraversal.js
@@ -15,16 +15,26 @@ const closeCard = document.querySelectorAll('.close')
 //     })
 // }
 
-closeCard.forEach(function (el) {
-    el.addEventListener('click', function (e) {
-        e.target.parentElement.style.display = 'none'
-    })
-})
+// closeCard.forEach(function (el) {
+//     el.addEventListener('click', function (e) {
+//         e.target.parentElement.style.display = 'none'
+//     })
+// })
 
-/* 
+/*
 parentNode, orang tua tapi nodenya keambil (enter)
 parentElement, orang tua
 nextSibling, previousSibling, sodara node
 nextElementSibling, previeousElementSibling, orang tua element
 */
 
+// Prevent Default => mencegah aksi default
+// a itu punya aksi default mengembalikan ke suatu halaman jadi harus di kasi pencegahan aksi default
+
+
+closeCard.forEach(function (el) {
+    el.addEventListener('click', function (e) {
+        e.target.parentElement.style.display = 'none'
+        e.preventDefault()
+    })
+})
