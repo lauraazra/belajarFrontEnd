a60d873e1b3b72ec6d7c7eaf7dc276e344c8bb15
lauraazra <lazraaprilyanti@gmail.com>
2026-02-24 02:44:46 +0700

    Object 1.3 prototype

--- javascriptLanjutan/object.js
+++ javascriptLanjutan/object.js
@@ -20,7 +20,7 @@ let mahasiswa2 = {
  */
 
 
-// Function Declaration -> tetap melakukan duplikasi
+// 2. Function Declaration -> tetap melakukan duplikasi
 /*
 function mahasiswa(nama, energi) {
     let mahasiswa = {}
@@ -45,7 +45,7 @@ let alvar = mahasiswa('alvar', 5)
 */
 
 
-// Construtor function -> akan digunakan terus
+// 3. Construtor function -> akan digunakan terus
 /*
 function Mahasiswa(nama, energi) {
     this.nama = nama
@@ -67,7 +67,8 @@ let alvar = new Mahasiswa('alvar', 15)
 */
 
 
-// Object Create
+// 4. Object Create
+/* 
 const methodMahasiswa = {
     makan: function (porsi) {
         this.energi += porsi;
@@ -97,3 +98,77 @@ function mahasiswa(nama, energi) {
 
 let azra = mahasiswa('azra', 5)
 let alvar = mahasiswa('alvar', 5)
+ */
+
+
+// Prototype
+// /* 
+// jadi begini bre, lu kalo pakae construtif ga bisa bikin kaya di atas. 
+
+// jadi this itu tuh sama kaya jsnya nulis
+// let this = objet.create(mahasiswa.protoype)
+// */
+
+// function Mahasiswa(nama, energi) {
+//     this.nama = nama
+//     this.energi = energi
+// }
+
+// // Mahasiswa.prototype.makan = function (porsi) {
+// //     this.energi += porsi;
+// //     console.log(`Selamat makan ${this.nama}`);
+// // }
+// // Mahasiswa.prototype.main = function (jam) {
+// //     this.energi -= jam;
+// //     console.log(`Selamat main ${this.nama}`);
+// // }
+// // Mahasiswa.prototype.tidur = function (jam) {
+// //     this.energi += 2 * jam;
+// //     console.log(`Selamat tidur ${this.nama}`);
+// // }
+
+// // atau lu bisa bikin gini
+// const methodMahasiswa = {
+//     makan: function (porsi) {
+//         this.energi += porsi;
+//         console.log(`Selamat makan ${this.nama}`);
+//     },
+//     main: function (jam) {
+//         this.energi -= jam;
+//         console.log(`Selamat main ${this.nama}`);
+//     },
+//     tidur: function (jam) {
+//         this.energi += 2 * jam;
+//         console.log(`Selamat tidur ${this.nama}`);
+//     },
+// }
+// // Mahasiswa.prototype.makan = methodMahasiswa.makan
+// // Mahasiswa.prototype.main = methodMahasiswa.main
+// // Mahasiswa.prototype.tidur = methodMahasiswa.tidur
+// // cara AI
+// Object.assign(Mahasiswa.prototype, methodMahasiswa);
+
+// let azra = new Mahasiswa('azra', 5)
+// let alvar = new Mahasiswa('alvar', 15)
+
+// Bagusnya pakai class
+class Mahasiswa {
+    constructor(nama, energi) {
+        this.nama = nama
+        this.energi = energi
+    }
+    makan(porsi) {
+        this.energi += porsi;
+        console.log(`Selamat makan ${this.nama}`);
+    }
+    main(jam) {
+        this.energi -= jam;
+        console.log(`Selamat main ${this.nama}`);
+    }
+    tidur(jam) {
+        this.energi += 2 * jam;
+        console.log(`Selamat tidur ${this.nama}`);
+    }
+}
+let azra = new Mahasiswa('azra', 10);
+let alvar = new Mahasiswa('alvar', 15);
