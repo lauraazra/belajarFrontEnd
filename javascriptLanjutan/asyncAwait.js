0d03b1702920527dd5795019346bff5db7398c3a
lauraazra <lazraaprilyanti@gmail.com>
2026-03-10 16:55:41 +0700

    Async Await

---
+++ javascriptLanjutan/asyncAwait.js
@@ -0,0 +1,42 @@
+/* 
+async function  => berjalan secara async
+                => Menghasilkan (implisit) promise
+                => cara penulisannya sama seperti function sync
+                => bisa memiliki Await = memberhentikan sementara fungsi sambil menunggu promisenya selesai
+*/
+
+// const coba = new Promise(resolve => {
+//     setTimeout(() => {
+//         resolve('selesai')
+//     }, 2000);
+// })
+// console.log(coba)
+// coba.then(() => console.log(coba))
+
+function cobaPromise() {
+    return new Promise((resolve, reject) => {
+        const waktu = 3000
+        if (waktu < 5000) {
+            setTimeout(() => {
+                resolve('selesai')
+            }, waktu);
+        } else {
+            reject('Kelamaan!')
+        }
+    })
+}
+
+// const coba = cobaPromise()
+// coba.then(() => console.log(coba))
+//     .catch(() => console.log(coba))
+
+async function cobaAsync() {
+    try {
+        const coba = await cobaPromise()
+        console.log(coba)
+    } catch (err) {
+        console.error(err)
+    }
+}
+
+cobaAsync()
