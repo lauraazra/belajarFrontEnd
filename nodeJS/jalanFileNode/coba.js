d449066e5b51f26333aa6a2c8ec461bb96317bb0
lauraazra <lazraaprilyanti@gmail.com>
2026-03-20 22:19:31 +0700

    Intro dan Menjalankan File Node

---
+++ nodeJS/jalanFileNode/coba.js
@@ -0,0 +1,7 @@
+// console.log('Hello World')
+
+const sayHello = (nama, umur) => `Hi, nama saya ${nama}, umur saya ${umur} tahun`
+
+// console.log(sayHello('Azra', 22))
+
+module.exports = sayHello;
---
+++ nodeJS/jalanFileNode/index.js
@@ -0,0 +1,7 @@
+const sayHello = require('./coba')
+console.log('Hello Azra')
+
+// sayHello('Azra', 22) // ga bisa diakses
+// kecuali eksekusinya ada di coba
+
+console.log(sayHello('Azra', 22))
