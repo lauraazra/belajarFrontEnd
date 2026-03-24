776c30b96ba215ca979685ee3891917b6fd36c54
lauraazra <lazraaprilyanti@gmail.com>
2026-03-24 10:38:25 +0700

    Contact App Readline

---
+++ nodeJS/11ContactAppReadline/app.js
@@ -0,0 +1,57 @@
+const contacts = require('./contacts')
+/* 
+bisa pakai object destructuring jadi
+{writeQuestion, savecontact}
+*/
+
+// fungsi utama readline
+const main = async () => {
+    const command = await contacts.writeQuestion('Masukan Perintah Anda: \n (add|update|list|detail|remove|removeall)')
+    switch (command.toLowerCase()) {
+        case 'add': {
+            const nama = await contacts.writeQuestion('Masukan nama Anda: ');
+            const noHp = await contacts.writeQuestion('Masukan no hp Anda: ');
+            const email = await contacts.writeQuestion('Masukan email Anda: ');
+            contacts.saveContact(nama, noHp, email)
+            break;
+        }
+
+        case 'update': {
+            const oldName = await contacts.writeQuestion('Masukan nama yang akan diupdate: ')
+            const newName = await contacts.writeQuestion('Masukan nama baru Anda: ')
+            const noHp = await contacts.writeQuestion('Masukan no hp baru Anda: ')
+            const email = await contacts.writeQuestion('Masukan email baru Anda: ')
+            contacts.updateContact(oldName, newName, noHp, email)
+            break;
+        }
+
+        case 'list': {
+            contacts.listContact()
+            break;
+        }
+
+        case 'detail': {
+            const nama = await contacts.writeQuestion('Masukan nama yang ingin dilihat detailnya')
+            contacts.detailContact(nama)
+            break;
+        }
+
+        case 'remove': {
+            const nama = await contacts.writeQuestion('Masukan nama yang akan dihapus')
+            contacts.removeContact(nama)
+            break;
+        }
+
+        case 'removeall': {
+            contacts.removeAllContact()
+            break;
+        }
+
+        default:
+            break;
+    }
+
+
+}
+
+main();
--- nodeJS/11ContactAppReadline/contacts.js
+++ nodeJS/11ContactAppReadline/contacts.js
@@ -1,6 +1,12 @@
 const fs = require('fs');
+const readline = require('node:readline');
 const validator = require('validator')
 const chalk = require('chalk')
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+})
 
 // membuat folder data jika belum ada
 const dirPath = './data'
@@ -14,30 +20,38 @@ if (!fs.existsSync(dataPath)) {
     fs.writeFileSync(dataPath, '[]', 'utf-8')
 }
 
+// Fungsi membuat pertanyaan
+const writeQuestion = (question) => {
+    return new Promise((resolve, reject) => {
+        rl.question(question, (value) => { resolve(value) })
+    })
+}
+
 // Validator
 const valid = ((noHp, email) => {
     const handleError = (msg) => {
         console.log(chalk.red.inverse(` ${msg} `));
         return false;
-    }
-    if (noHp && noHp !== '-' && !validator.isMobilePhone(noHp, 'id-ID')) return handleError('No HP salah!')
-    if (email && email !== '-' && !validator.isEmail(email)) return handleError('Email salah!')
+    };
+    if (noHp && noHp !== '-' && !validator.isMobilePhone(noHp, 'id-ID')) return handleError('No HP salah!');
+    if (email && email !== '-' && !validator.isEmail(email)) return handleError('Email salah!');
 
     return true
 })
 
-// load contact.json
-const loadContact = () => {
+// read contact.json
+const readContact = () => {
     const file = fs.readFileSync('data/contacts.json', 'utf8')
     const contactData = JSON.parse(file)
     return contactData
 }
 
-// Cek Duplikasi
-const checkDuplicate = (contactData, nama) => {
-    const duplicate = contactData.find((c) => c.nama === nama)
+// Check Duplikasi
+const checkDuplicate = (contactsData, nama) => {
+    const duplicate = contactsData.find(c => c.nama === nama)
     if (duplicate) {
         console.log(chalk.yellow.inverse(' Nama sudah terdaftar, gunakan nama lain! '));
+        rl.close()
         return true
     }
     return false
@@ -45,9 +59,13 @@ const checkDuplicate = (contactData, nama) => {
 
 // Fungsi menyimpan kontak
 const saveContact = (nama, noHp, email) => {
-    if (!valid(noHp, email)) return false;
+    if (!valid(noHp, email)) {
+        rl.close();
+        return false;
+    }
 
-    const contactData = loadContact()
+    const contactData = readContact()
+
     if (checkDuplicate(contactData, nama)) return false
 
     const contact = { nama, noHp, email }
@@ -55,20 +73,26 @@ const saveContact = (nama, noHp, email) => {
 
     fs.writeFileSync('data/contacts.json', JSON.stringify(contactData))
     console.log(chalk.green.inverse.bold(`Data ${nama} berhasil disimpan!`))
+    rl.close()
 }
 
-// Update 1 nama
-const contactUpdate = (oldName, newName, noHp, email) => {
-    const contactsData = loadContact()
+// UpdateContact 1 nama
+const updateContact = (oldName, newName, noHp, email) => {
+    const contactsData = readContact()
 
     const index = contactsData.findIndex(c => c.nama === oldName)
 
     if (index == -1) {
         console.log(chalk.yellow.inverse(` Data ${oldName} tidak ditemukan `));
+        rl.close()
         return false
     }
 
-    if (!valid(noHp, email)) return false;
+    if (!valid(noHp, email)) {
+        rl.close();
+        return false;
+    }
+
     if (checkDuplicate(contactsData, newName)) return false
 
     contactsData[index].nama = newName || contactsData[index].nama
@@ -77,50 +101,57 @@ const contactUpdate = (oldName, newName, noHp, email) => {
 
     fs.writeFileSync('data/contacts.json', JSON.stringify(contactsData))
     console.log(chalk.green.inverse(`Data ${oldName} berhasil diubah!`))
+    rl.close()
 }
 
 // Melihat isi contact
-const contactList = () => {
-    const contactData = loadContact()
+const listContact = () => {
+    const contactData = readContact()
     console.log(chalk.cyan.inverse(`List Kontak`))
     console.table(contactData);
     process.exit()
 }
 
-// Lihat detail
-const contactDetail = (nama) => {
-    const contactsData = loadContact()
+// Lihat detailContact
+const detailContact = (nama) => {
+    const contactsData = readContact()
 
     const newContacts = contactsData.find((c) => c.nama === nama)
 
     if (!newContacts) {
         console.log(`Data ${nama} tidak ditemuka`)
+        rl.close()
         return false
     }
 
     console.log(chalk.cyan.inverse(`Detail data ${nama}`))
-    console.table(newContacts)
+    console.table(newContacts);
+    rl.close()
+
 }
 
 // Menghapus 1 nama
-const contactRemove = (nama) => {
-    const contactsData = loadContact()
+const removeContact = (nama) => {
+    const contactsData = readContact()
 
     const newContacts = contactsData.filter((c) => c.nama !== nama)
 
     if (newContacts.length === contactsData.length) {
         console.log(`data ${nama}, tidak dapat ditemukan`)
+        rl.close()
         return false
     }
 
     fs.writeFileSync('data/contacts.json', JSON.stringify(newContacts))
     console.log(chalk.red.bold(`Kontak ${nama} berhasil dihapus!`))
+    rl.close()
 }
 
 // Menghapus semua
-const contactRemoveAll = () => {
+const removeAllContact = () => {
     fs.writeFileSync('data/contacts.json', '[]')
     console.log(chalk.red.bold(' Semua kontak berhasil dihapus! '))
+    rl.close()
 }
 
-module.exports = { saveContact, contactList, contactDetail, contactRemove, contactUpdate, contactRemoveAll }
+module.exports = { writeQuestion, saveContact, listContact, detailContact, removeContact, updateContact, removeAllContact }
